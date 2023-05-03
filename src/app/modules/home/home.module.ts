b00e9430f7b829a9588c7598c4c890b6321594f1
ArianSanzo <ariansanzo@gmail.com>
2023-05-03 16:38:10 -0300

    Creación de componentes principales

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/header/banner/banner.component';
+import { LoginButtonComponent } from './components/header/login-button/login-button.component';
+import { LogosComponent } from './components/header/logos/logos.component';
+import { SocialNetworksComponent } from './components/header/social-networks/social-networks.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    LoginButtonComponent,
+    LogosComponent,
+    SocialNetworksComponent
   ],
   imports: [
     CommonModule
