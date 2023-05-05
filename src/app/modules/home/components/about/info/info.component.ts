f0e862bcf1c8752d06b91a6831c0a3976f8f080e
ArianSanzo <ariansanzo@gmail.com>
2023-05-05 15:23:23 -0300

    Completado con data binding

--- src/app/modules/home/components/about/info/info.component.ts
+++ src/app/modules/home/components/about/info/info.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent {
-
+  name: string = "Arián Ignacio Sanzo";
+  title: string = "Full Stack Developer Jr";
+  city: string = "Rosario, Santa Fe, Argentina";
+  description: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nisi esse a, saepe accusantium quo officia dolore nesciunt enim minima";
 }
--- src/app/modules/home/components/about/photo/photo.component.ts
+++ src/app/modules/home/components/about/photo/photo.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./photo.component.css']
 })
 export class PhotoComponent {
-
+  photo : string = "https://c.pxhere.com/photos/e8/60/smile_profile_face_male_portrait_young_person_glasses-451653.jpg!d"; 
 }
