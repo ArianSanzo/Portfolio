93bc14a721c3fe53e3fb454671e332146c996f3e
ArianSanzo <ariansanzo@gmail.com>
2023-05-10 17:32:51 -0300

    Completado de componentes

--- src/app/modules/home/components/proyects/proyect/proyect.component.ts
+++ src/app/modules/home/components/proyects/proyect/proyect.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyect.component.css']
 })
 export class ProyectComponent {
-
+  protected name: string="Portfolio";
+  protected description: string="Mi portafolio virtual";
+  protected releaseDate: string="2023";
+  protected image: string="";
+  protected url: string="";
 }
