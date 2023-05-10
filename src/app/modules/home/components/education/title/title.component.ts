90eedda551aabb21b70a9c14f140e0c5269d820d
ArianSanzo <ariansanzo@gmail.com>
2023-05-10 16:06:07 -0300

    Completado de componente education y tittle

--- src/app/modules/home/components/education/title/title.component.ts
+++ src/app/modules/home/components/education/title/title.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent {
+  protected logo: string = "";
+  protected title: string = "Estudiante";
+  protected place: string = "UNR";
+  protected state: boolean = true;
+  protected startDate: string = "2000";
+  protected endDate: string = "2005";
+  protected info: string = "Descripción cortita.";
 
 }
